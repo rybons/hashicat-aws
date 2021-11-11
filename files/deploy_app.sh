9b4f4142921dc471e6e7cab84bf534daa5cb7a9c
Siraj Rauff <sirajrauff@gmail.com>
2021-11-11 14:52:13 -0500

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Home of Jenkins, the ultimate DevOps tool
+  Welcome to ${PREFIX}'s app. Home of Jenkins, the ultimate DevOps tool
   <!-- END -->
 
   </div>
